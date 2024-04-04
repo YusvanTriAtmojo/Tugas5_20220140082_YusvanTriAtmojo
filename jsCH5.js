d2b908b30bf25a9f512728d59c3038a79583a53f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-04-04 17:39:06 +0700

    my first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini MV NMIXX berjudul 0.0");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini audio/musik NMIXX berjudul 0.0");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar NMIXX");
+});
